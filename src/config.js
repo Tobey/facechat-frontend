50525f594ecf6b1474435fdddedfe3b420a05ce1
Tobey Asinugo <tobey.asinugo@gmail.com>
2020-08-31 21:02:31 +0100

    home wip

--- src/config.js
+++ src/config.js
@@ -1,10 +1,35 @@
-export default {
-  PAYTOK_URL: 'http://localhost:8002',
-  PAYTOK_WS_URL: 'ws://localhost:8002',
+
+
+const getEnvVairables = () => {
+  let config = {}
+
+  if (typeof (process.env.PAYTOK_URL) == 'undefined') {
+    console.log("default PAYTOK_URL")
+    config['PAYTOK_URL'] = 'http://localhost:8002'
+  } else {
+    config['PAYTOK_URL'] = process.env.PAYTOK_URL
+  }
+
+  if (typeof (process.env.PAYTOK_WS_URL) == 'undefined') {
+    console.log("default PAYTOK_WS_URL")
+    config['PAYTOK_WS_URL'] = 'ws://localhost:8002'
+  } else {
+    config['PAYTOK_WS_URL'] = process.env.PAYTOK_WS_URL
+  }
+
+  return config
+}
+
+
+const config = {
+  ...getEnvVairables(),
   API_KEY: '46531532',
   // SESSION_ID: '',
   // TOKEN: ''
   SESSION_ID: '2_MX40NjUzMTUzMn5-MTU4NjYwNjUzNzcwOH5UZ0szUk9LMUNCbWx4OVBvRS83dWFwSW9-fg',
   TOKEN: 'T1==cGFydG5lcl9pZD00NjUzMTUzMiZzaWc9ZjBiYmJkODJjMzYzODhlZWI2YWQyMmU5N2RiZDgzM2E4OWNlM2ZlZDpzZXNzaW9uX2lkPTJfTVg0ME5qVXpNVFV6TW41LU1UVTROall3TmpVek56Y3dPSDVVWjBzelVrOUxNVU5DYld4NE9WQnZSUzgzZFdGd1NXOS1mZyZjcmVhdGVfdGltZT0xNTg2NjA2NTQ1Jm5vbmNlPTAuNDg5ODg2MDI2MzA4MDI3MTMmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTU4NjYxMDE0NSZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ=='
 };
- 
+console.log(config)
+console.log(process.env)
+
+export default config
--- src/pages/home.js
+++ src/pages/home.js
@@ -57,7 +57,7 @@ const HomepageHeading = ({ mobile }) => {
     />
     <Header
       as='h2'
-      content='Do whatever you want when you want to.'
+      content='A video streaming platform for social media infulencers'
       inverted
       style={{
         fontSize: mobile ? '1.5em' : '1.7em',
@@ -246,33 +246,31 @@ const HomepageLayout = () => {
         <Grid.Row>
           <Grid.Column width={8}>
             <Header as='h3' style={{ fontSize: '2em' }}>
-              We Help Companies and Companions
+              We Help Influnecer connect with fans
             </Header>
             <p style={{ fontSize: '1.33em' }}>
-              We can give your company superpowers to do things that they never thought possible.
-              Let us delight your customers and empower your needs... through pure data analytics.
+              Think Onlyfans but better
             </p>
             <Header as='h3' style={{ fontSize: '2em' }}>
-              We Make Bananas That Can Dance
+              We've made a platform to monetize video call
             </Header>
             <p style={{ fontSize: '1.33em' }}>
-              Yes that's right, you thought it was the stuff of dreams, but even bananas can be
-              bioengineered.
+              Yes that's right, you can make alot of money here
             </p>
           </Grid.Column>
           <Grid.Column floated='right' width={6}>
-            <Image bordered rounded size='large' src={`${SemanticStaticUrl}/images/wireframe/white-image.png`} />
+            <Image bordered rounded size='large' src="https://bagisto.com/wp-content/uploads/2019/10/coming-soon-2579123_960_720-1.jpg" />
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
           <Grid.Column textAlign='center'>
-            <MobileButton size='huge'>Check Them Out</MobileButton>
+            <MobileButton size='huge'>Register interest</MobileButton>
           </Grid.Column>
         </Grid.Row>
       </Grid>
     </Segment>
 
-    <Segment style={{ padding: '0em' }} vertical>
+    {/* <Segment style={{ padding: '0em' }} vertical>
       <Grid celled='internally' columns='equal' stackable>
         <Grid.Row textAlign='center'>
           <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
@@ -329,9 +327,9 @@ const HomepageLayout = () => {
           I'm Still Quite Interested
         </MobileButton>
       </Container>
-    </Segment>
+    </Segment>*/}
 
-    <Segment inverted vertical style={{ padding: '5em 0em' }}>
+    <Segment inverted vertical style={{ padding: '5em 0em' }}> 
       <Container>
         <Grid divided inverted stackable>
           <Grid.Row>
@@ -340,25 +338,21 @@ const HomepageLayout = () => {
               <List link inverted>
                 <List.Item as='a'>Sitemap</List.Item>
                 <List.Item as='a'>Contact Us</List.Item>
-                <List.Item as='a'>Religious Ceremonies</List.Item>
-                <List.Item as='a'>Gazebo Plans</List.Item>
               </List>
             </Grid.Column>
             <Grid.Column width={3}>
               <Header inverted as='h4' content='Services' />
               <List link inverted>
-                <List.Item as='a'>Banana Pre-Order</List.Item>
-                <List.Item as='a'>DNA FAQ</List.Item>
-                <List.Item as='a'>How To Access</List.Item>
-                <List.Item as='a'>Favorite X-Men</List.Item>
+                <List.Item as='a'>facechat FAQs</List.Item>
+                <List.Item as='a'>About us</List.Item>
               </List>
             </Grid.Column>
             <Grid.Column width={7}>
               <Header as='h4' inverted>
-                Footer Header
+                Mantra
               </Header>
               <p>
-                Extra space for a call to action inside the footer that could help re-engage users.
+                Time is money. call me on Facechat!
               </p>
             </Grid.Column>
           </Grid.Row>
